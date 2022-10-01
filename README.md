c5552b50a3203d50f6d0e41bdd3e185b0696fc7a
jasonxyliu <xinyu_liu@brown.edu>
2022-10-01 16:49:56 -0400

    added gpt3 api

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-#Installation
+# Installation
 ```
-$conda create -n  lang2ltl python=3.9 dill
+$conda create -n lang2ltl python=3.9 dill
 $conda activate lang2ltl
+$pip install openai
+```
+Optional if use spaCy
+```
 $pip install -U pip setuptools wheel
 $pip install -U spacy
 $python -m spacy download en_core_web_sm
+```
+
+Set environment variable for API key
+```
+export OPENAI_API_KEY=<YOUR_API_KEY>
 ```
--- gpt3.py
+++ gpt3.py
@@ -1,11 +1,20 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 class GPT3:
-    def __init__(self):
-        return
+    def __init__(self, temp=0.6):
+        self.temp = temp
 
     def generate(self, query_prompt):
-        return ""
+        response = openai.Completion.create(
+            model="text-davinci-002",
+            prompt=query_prompt,
+            temperature=self.temp,
+        )
+        return response.choices[0].text
 
     def extract(self, query_prompt):
         out = self.generate(query_prompt)
@@ -21,3 +30,27 @@ class GPT3:
     def translate(self, query_prompt):
         out = self.generate(query_prompt)
         return out
+
+
+if __name__ == '__main__':
+    gpt3 = GPT3()
+
+    query_prompt = \
+        "English: Go to Bookstore then to Science Library\n" \
+        "Landmarks: Bookstore | Science Library\n" \
+        "LTL: F ( Bookstore & F ( Science Library ) )\n\n" \
+        "English: Go to Bookstore then reach Science Library\n" \
+        "Landmarks: Bookstore | Science Library\n" \
+        "LTL: F ( Bookstore & F ( Science Library ) )\n\n" \
+        "English: Find Bookstore then go to Science Library\n" \
+        "Landmarks: Bookstore | Science Library\n" \
+        "LTL: F ( Bookstore & F ( Science Library ) )\n\n" \
+        "English: Go to Burger Queen then to black stone park, but after KFC\n" \
+        "Landmarks: Burger Queen | black stone park | KFC\n" \
+        "LTL: F ( Burger Queen & F ( KFC & F ( black stone park ) )\n\n" \
+        "English: Go to Burger Queen then to black stone park; go to KFC before black stone park and after Burger Queen\n"\
+        "Landmarks: Burger Queen | black stone park | KFC\n" \
+        "LTL: F ( Burger Queen & F ( KFC & F ( black stone park ) )\n\n" \
+        "English: "
+
+    gpt3.generate(query_prompt)
