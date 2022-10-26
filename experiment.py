631a5228a2f9eb87db62a11c9608a95c7e77c027
jasonxyliu <xinyu_liu@brown.edu>
2022-10-26 10:25:23 -0400

    added cache name embed

--- experiment.py
+++ experiment.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import numpy as np
 import spot
@@ -29,8 +30,8 @@ def run_exp():
             'NER': utt2names if not args.full_e2e else None,
             'Grounding': name2grounds if not args.full_e2e else None,
             'Placeholder maps': placeholder_maps if not (args.translate_e2e or args.full_e2e) else None,
-            'Symbolic LTLs': symbolic_ltls if not (args.translate_e2e or args.full_e2e) else None,
             'Input utterances': input_utts,
+            'Symbolic LTLs': symbolic_ltls if not (args.translate_e2e or args.full_e2e) else None,
             'Output LTLs': output_ltls,
             'Ground truth': true_ltls
         }
@@ -74,7 +75,11 @@ def grounding(names):
     """
     Find groundings (objects in given environment) of name entities in input utterances.
     """
-    name2embed = load_from_file(args.name_embed)
+    obj2embed = load_from_file(args.obj_embed)  # load embeddings of known objects in given environment
+    if os.path.exists(args.name_embed):  # load cached embeddings of name entities
+        name2embed = load_from_file(args.name_embed)
+    else:
+        name2embed = {}
 
     if args.ground == 'gpt3':
         ground_module = GPT3()
@@ -83,12 +88,21 @@ def grounding(names):
     else:
         raise ValueError(f'ERROR: grounding module not recognized: {args.ground}')
 
-    name2grounds = {}
+    name2grounds, is_add_embed = {}, False
     for name in names:
-        embed = ground_module.get_embedding(name, args.engine)
-        sims = {n: cosine_similarity(e, embed) for n, e in name2embed.items()}
+        if name in name2embed:  # use cached embedding if exists
+            embed = name2embed[name]
+        else:
+            embed = ground_module.get_embedding(name, args.engine)
+            name2embed[name] = embed
+            is_add_embed = True
+        sims = {n: cosine_similarity(e, embed) for n, e in obj2embed.items()}
         sims_sorted = sorted(sims.items(), key=lambda kv: kv[1], reverse=True)
         name2grounds[name] = list(dict(sims_sorted[:args.topk]).keys())
+
+        if is_add_embed:
+            save_to_file(name2embed, args.name_embed)
+
     return name2grounds
 
 
@@ -147,7 +161,7 @@ def evaluate_lang(output_ltls, true_ltls):
         try:  # output LTL formula may have syntax error
             accs.append(spot.are_equivalent(spot.formula(out_ltl), spot.formula(true_ltl)))
         except SyntaxError:
-            print(f'Syntax error in output LTL: {out_ltl}')
+            print(f'Syntax error in output LTL: {out_ltl}\n')
             accs.append(False)
     acc = np.mean(accs)
     return acc
@@ -174,7 +188,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, default='data/test_src_corlw.txt', help='file path to input utterances')
     parser.add_argument('--true_ltls', type=str, default='data/test_tar_corlw.txt', help='path to true grounded LTL formulas')
-    parser.add_argument('--nsamples', type=int, default=20, help='use the first nsamples number of samples')
+    parser.add_argument('--nsamples', type=int, default=5, help='use the first nsamples number of samples')
     parser.add_argument('--true_trajs', type=str, default='data/true_trajs.pkl', help='path to true trajectories')
     parser.add_argument('--full_e2e', action='store_true', help="solve translation and ground end-to-end using GPT-3")
     parser.add_argument('--full_e2e_prompt', type=str, default='data/full_e2e_prompt_10.txt', help='path to full end-to-end prompt')
@@ -185,10 +199,11 @@ if __name__ == '__main__':
     parser.add_argument('--trans', type=str, default='gpt3', choices=['gpt3', 's2s_sup', 's2s_weaksup'], help='translation module')
     parser.add_argument('--trans_prompt', type=str, default='data/trans_prompt_10.txt', help='path to trans prompt')
     parser.add_argument('--ground', type=str, default='gpt3', choices=['gpt3', 'bert'], help='grounding module')
-    parser.add_argument('--name_embed', type=str, default='data/name2embed_davinci.json', help='path to known name embedding')
+    parser.add_argument('--obj_embed', type=str, default='data/obj2embed_davinci.json', help='path to embedding of objects in env')
+    parser.add_argument('--name_embed', type=str, default='data/name2embed_davinci.pkl', help='path to embedding of names in language')
     parser.add_argument('--topk', type=int, default=2, help='top k similar known names to name entity')
     parser.add_argument('--engine', type=str, default='davinci', choices=['ada', 'babbage', 'curie', 'davinci'], help='gpt-3 engine')
-    parser.add_argument('--save_result_path', type=str, default='data/test_result_modular_20.json', help='file path to save outputs of each model in a json file')
+    parser.add_argument('--save_result_path', type=str, default='data/test_result_modular_5.json', help='file path to save outputs of each model in a json file')
     args = parser.parse_args()
 
     input_utts = load_from_file(args.input)[:args.nsamples]
