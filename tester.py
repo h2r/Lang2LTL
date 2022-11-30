25e178beefff3dc7c675ef90ad140b8d2cc85f79
jasonxyliu <xinyu_liu@brown.edu>
2022-11-29 22:24:40 -0500

    added, updated tests for space around binary op

--- tester.py
+++ tester.py
@@ -11,13 +11,16 @@ class TestUtils(unittest.TestCase):
         in_out_pairs = [
             ("a", "a"),
             ("! a", "!(a)"),
-            ("& a b", "(a&b)"),
-            ("U ! a b", "(!(a)Ub)"),
-            ("-> a b", "(a->b)"),
-            ("<-> a b", "(a<->b)"),
+            ("& a b", "(a & b)"),
+            ("U ! a b", "(!(a) U b)"),
+            ("-> a b", "(a -> b)"),
+            ("<-> a b", "(a <-> b)"),
             ("G F X ! a", "G(F(X(!(a))))"),
-            ("& F & a F b G ! c", "(F((a&F(b)))&G(!(c)))"),
-            ("& F & a F b -> a U ! a b", "(F((a&F(b)))&(a->(!(a)Ub)))")
+            ("& F & a F b G ! c", "(F((a & F(b))) & G(!(c)))"),
+            ("& F & a F b -> a U ! a b", "(F((a & F(b))) & (a -> (!(a) U b)))"),
+            ("G -> b X U b & ! b U ! b a", "G((b -> X((b U (!(b) & (!(b) U a))))))"),
+            ("& G -> b X U b & ! b U ! b a G -> c X U c & ! c U ! c b",
+             "(G((b -> X((b U (!(b) & (!(b) U a)))))) & G((c -> X((c U (!(c) & (!(c) U b)))))))"),
         ]
         for prefix_formula, infix_formula in in_out_pairs:
             output_formula = prefix_to_infix(prefix_formula)
