c26c8fc9df5564049506f020dc70a59709e5bb83
jasonxyliu <xinyu_liu@brown.edu>
2023-01-01 21:39:37 -0500

    bug fix; added logprobs in gpt3 output
    fixed to have more than 1 out seqs when n > 1
    added logprobs to top 5 tokens every step

--- gpt3.py
+++ gpt3.py
@@ -9,26 +9,32 @@ openai.organization = os.getenv("ORG_ID")
 class GPT3:
     def extract_ne(self, query, **kwargs):
         query_prompt = kwargs["prompt"] + query + "\nLandmarks:"
-        out = GPT3.generate(query_prompt)
-        name_entities = out.split(' | ')
+        outs = GPT3.generate(query_prompt)
+        name_entities = outs[0].split(' | ')
         return name_entities
 
     def translate(self, query, **kwargs):
         query_prompt = kwargs["prompt"] + query + "\nLTL:"
         outs = GPT3.generate(query_prompt, n=kwargs["n"])
-        if kwargs["n"] > 1:  # GPT-3 generates more than 1 candidate translations
+        if kwargs["n"] == 1:
+            return outs[0].strip()
+        else:  # GPT-3 generates more than 1 candidate translations
             return [out.strip() for out in outs]
-        return outs.strip()
 
     @staticmethod
     def generate(query_prompt, engine="text-davinci-003", temp=0.6, n=1):  # engines must match when compare two embeddings
-        response = openai.Completion.create(
+        raw_responses = openai.Completion.create(
             model=engine,
             prompt=query_prompt,
             temperature=temp,
-            n=n
-        )['choices'][0]['text']
-        return response
+            n=n,
+            logprobs=5
+        )
+        if n == 1:
+            responses = [raw_responses['choices'][0]['text']]
+        else:
+            responses = [choice['text'] for choice in raw_responses['choices']]
+        return responses
 
     @staticmethod
     @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
