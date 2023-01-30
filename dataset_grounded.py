de0b773f12f0c55ed90e13900790a9006714983d
jasonxyliu <xinyu_liu@brown.edu>
2023-01-29 20:42:52 -0500

    added rand sample nsamples lmks from lmk perm after remove prop perms

--- dataset_grounded.py
+++ dataset_grounded.py
@@ -5,11 +5,12 @@ import argparse
 import os
 from pathlib import Path
 import string
+from itertools import permutations
 import random
 from collections import defaultdict
 from pprint import pprint
 
-from utils import load_from_file, save_to_file, substitute_single_letter
+from utils import load_from_file, save_to_file, substitute_single_letter, name_to_prop
 from formula_sampler import PROPS
 
 
@@ -43,7 +44,7 @@ def rename_map_files(osm_lmks_dpath):
                 os.rename(map_fpath, new_map_fpath)
 
 
-def construct_lmk2prop(osm_lmks_dpath):
+def construct_lmk2prop(osm_lmks_dpath, model):
     """
     For testing conversion of every landmark name to proposition and inspecting OSM landmark names.
     Construct landmark names to propositions mapping.
@@ -57,83 +58,89 @@ def construct_lmk2prop(osm_lmks_dpath):
         lmk_json = load_from_file(lmk_fpath)
         lmk_names = lmk_json.keys()
         for lmk_name in lmk_names:
-            lmk2prop[lmk_name] = lmk_to_prop(lmk_name)
+            lmk2prop[lmk_name] = name_to_prop(lmk_name, model)
         pprint(lmk2prop)
         print("\n")
 
 
-def lmk_to_prop(lmk_name):
-    """
-    :param lmk_name: landmark name, e.g. Canal Street, TD Bank.
-    :return: proposition that corresponds to input landmark name and is compatible with Spot.
-    """
-    return "_".join(lmk_name.translate(str.maketrans('/()-–', '     ', "'’,.!?")).lower().split())
-
-
-def lmk_to_prop_copynet(lmk_name):
-    """
-    :param lmk_name: landmark name, e.g. Canal Street, TD Bank.
-    :return: proposition in specific form required by CopyNet.
-    """
-    return f"lm( {lmk_name} )lm"
-
-
-def construct_osm_dataset(split_dpath, osm_lmks, city, seed, remove_perm, model, save_dpath):
+def construct_grounded_dataset(split_dpath, lmks, city, remove_perm, seed, nsamples, model, save_dpath):
     print(f"Constructing grounded dataset for city: {city}")
     split_fnames = sorted([fname for fname in os.listdir(split_dpath) if "pkl" in fname])
     save_dpath = os.path.join(save_dpath, "grounded", city)
     os.makedirs(save_dpath, exist_ok=True)
 
+    nprops2lmkperms = {} if nsamples else None
+
     for split_fname in split_fnames:
         print(split_fname)
         dataset = load_from_file(os.path.join(split_dpath, split_fname))
         train_iter, train_meta, valid_iter, valid_meta = dataset["train_iter"], dataset["train_meta"], dataset["valid_iter"], dataset["valid_meta"]
-        print(f"before remove_perm: {len(valid_iter)} {len(valid_meta)}")
-        if remove_perm:
+
+        print(f"old train, test size: {len(train_iter)} {len(train_meta)} {len(valid_iter)} {len(valid_meta)}")
+        print(f"test size before remove_perm: {len(valid_iter)} {len(valid_meta)}")
+        if remove_perm:  # remove prop perms from valid_iter
             valid_iter_noperm, valid_meta_noperm = [], []
-            formula2data = defaultdict(set)  # keep unique utts
+            formula2data = defaultdict(list)
             for (utt, ltl), (pattern_type, props) in zip(valid_iter, valid_meta):
                 props = list(props)
                 sub_map = {old_prop: new_prop for old_prop, new_prop in zip(props, PROPS[:len(props)])}  # remove perm
                 utt_noperm = substitute_single_letter(utt, sub_map)
                 ltl_noperm = substitute_single_letter(ltl, sub_map)
-                formula2data[(pattern_type, len(props))].add((utt_noperm, ltl_noperm))
+                formula2data[(pattern_type, len(props))].append((utt_noperm, ltl_noperm))
             for (pattern_type, nprops), data in formula2data.items():
+                data = list(dict.fromkeys(data))  # unique utt structures per formula in data. same order across runs
                 for utt, ltl in data:
                     valid_iter_noperm.append((utt, ltl))
                     valid_meta_noperm.append((pattern_type, PROPS[:nprops]))
             valid_iter, valid_meta = valid_iter_noperm, valid_meta_noperm
-        print(f"num of unique formulas: {len(set([(pattern_type, len(props)) for pattern_type, props in valid_meta]))}")
-        print(f"unique formulas:\n{set([(pattern_type, len(props)) for pattern_type, props in valid_meta])}")
-        print(f"after remove_perm: {len(valid_iter)} {len(valid_meta)}")
-        dataset["train_iter"], dataset["train_meta"] = substitute_lmks(train_iter, train_meta, osm_lmks, seed, model)
-        dataset["valid_iter"], dataset["valid_meta"] = substitute_lmks(valid_iter, valid_meta, osm_lmks, seed+10000000, model)  # +10000000 avoid sampele lmks w/ same seeds as train set
+        unique_formulas = set([(pattern_type, len(props)) for pattern_type, props in valid_meta])
+        print(f"num of unique formulas: {len(unique_formulas)}")
+        print(f"unique formulas:\n{unique_formulas}")
+        print(f"test size after remove_perm: {len(valid_iter)} {len(valid_meta)}")
+
+        dataset["train_iter"], dataset["train_meta"] = substitute_lmks(train_iter, train_meta, lmks, seed, model)
+        dataset["valid_iter"], dataset["valid_meta"] = substitute_lmks(valid_iter, valid_meta, lmks, seed+10000000, model, nprops2lmkperms, nsamples)  # +10000000 avoid sampele lmks w/ same seeds as train set
         dataset["city"], dataset["seed_lmk"], dataset["remove_perm"], dataset["model"] = city, seed, remove_perm, model
         save_to_file(dataset, os.path.join(save_dpath, split_fname))
 
+        print(f"new train, test size: {len(train_iter)} {len(train_meta)} {len(valid_iter)} {len(valid_meta)}")
 
-def substitute_lmks(data, meta_data, osm_lmks, seed, model):
+
+def substitute_lmks(data, meta_data, lmks, seed, model, nprops2lmkperms, nsamples=None):
     data_grounded, meta_data_grounded = [], []
+
     for idx, ((utt, ltl), (pattern_type, props)) in enumerate(zip(data, meta_data)):
         props = [prop.translate(str.maketrans('', '', string.punctuation)).strip() for prop in props]  # ['a,'] -> ['a']
         seed += idx  # diff seed to sample diff lmks for each utt-ltl pair
-        utt_grounded, ltl_grounded, lmk_names = substitute_lmk(utt, ltl, osm_lmks, props, seed, model)
-        if utt == utt_grounded or ltl == ltl_grounded:
-            raise ValueError(f"ERROR\n{utt}=={utt_grounded}\n{ltl}=={ltl_grounded}")
-        data_grounded.append((utt_grounded, ltl_grounded))
-        meta_data_grounded.append((utt, ltl, pattern_type, props, lmk_names, seed))
+        if nsamples:  # permute lmks for valid_iter
+            if len(props) not in nprops2lmkperms:
+                nprops2lmkperms[len(props)] = list(permutations(lmks, len(props)))  # each perm contain len(props) lmks
+            lmk_perms = nprops2lmkperms[len(props)]
+            random.seed(seed)
+            lmk_subs = random.sample(lmk_perms, nsamples)  # sample nsamples lmk perms
+            breakpoint()
+        else:
+            lmk_subs = [lmks]
+        for lmk_sub in lmk_subs:
+            utt_grounded, ltl_grounded, lmk_names = substitute_lmk(utt, ltl, lmk_sub, props, seed, model)
+            if utt == utt_grounded or ltl == ltl_grounded:
+                raise ValueError(f"ERROR\n{utt}=={utt_grounded}\n{ltl}=={ltl_grounded}")
+            data_grounded.append((utt_grounded, ltl_grounded))
+            meta_data_grounded.append((utt, ltl, pattern_type, props, lmk_names, seed))
     return data_grounded, meta_data_grounded
 
 
-def substitute_lmk(utt, ltl, osm_lmks, props, seed, model):
-    random.seed(seed)
-    lmk_names = random.sample(osm_lmks, len(props))
+def substitute_lmk(utt, ltl, lmks, props, seed, model):
+    if len(lmks) == len(props):  # already sample lmks from all perms for valid_iter
+        lmk_names = lmks
+    else:  # randomly sample lmks from complete lmk list for train_iter
+        random.seed(seed)
+        lmk_names = random.sample(lmks, len(props))
 
     sub_map = {prop: name for prop, name in zip(props, lmk_names)}
     utt_grounded = substitute_single_letter(utt, sub_map)
 
-    lmk_to_prop_fn = lmk_to_prop if model == "lang2ltl" else lmk_to_prop_copynet
-    sub_map = {prop: lmk_to_prop_fn(name) for prop, name in zip(props, lmk_names)}
+    sub_map = {prop: name_to_prop(name, model) for prop, name in zip(props, lmk_names)}
     ltl_grounded = substitute_single_letter(ltl, sub_map)
 
     return utt_grounded, ltl_grounded, lmk_names
@@ -142,20 +149,22 @@ def substitute_lmk(utt, ltl, osm_lmks, props, seed, model):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--split_dpath", type=str, default="data/holdout_split_batch12_perm", help="dpath to all split datasets.")
-    parser.add_argument("--city", type=str, default="all", help="city landmarks from json files in data/osm/osm_lmks.")
+    parser.add_argument("--env", type=str, default="osm", choices=["osm", "cleanup"], help="environment name.")
+    parser.add_argument("--city", type=str, default="boston", help="city landmarks from 1 or all json files in data/osm/osm_lmks.")
     parser.add_argument("--seed", type=int, default=42, help="random seed to sample lmks.")
+    parser.add_argument("--nsamples", type=int, default=15, help="number of samples per utt strucutre when perm lmks for valid set.")
     parser.add_argument("--remove_perm", action="store_true", help="True to remove permutations in validation set.")
-    parser.add_argument("--model", type=str, default="copynet", choices=["lang2ltl", "copynet"], help="fpath to symbolic dataset.")
+    parser.add_argument("--model", type=str, default="copynet", choices=["lang2ltl", "copynet"], help="model name.")
     args = parser.parse_args()
 
-    osm_dpath = os.path.join("data", "osm")
-    osm_lmks_dpath = os.path.join(osm_dpath, "lmks")
-    # construct_lmk2prop(osm_lmks_dpath)  # for testing
-    if args.city == "all":
-        cities = [os.path.splitext(fname)[0] for fname in os.listdir(osm_lmks_dpath) if "json" in fname]
-    else:
-        cities = [args.city]
-
-    for city in cities:
-        osm_lmks = list(load_from_file(os.path.join(osm_lmks_dpath, f"{city}.json")).keys())
-        construct_osm_dataset(args.split_dpath, osm_lmks, city, args.seed, args.remove_perm, args.model, osm_dpath)
+    env_dpath = os.path.join("data", args.env)
+    env_lmks_dpath = os.path.join(env_dpath, "lmks")
+    # construct_lmk2prop(osm_lmks_dpath, args.model)  # for testing
+    if args.env == "osm":
+        if args.city == "all":
+            cities = [os.path.splitext(fname)[0] for fname in os.listdir(env_lmks_dpath) if "json" in fname]
+        else:
+            cities = [args.city]
+        for city in cities:
+            city_lmks = list(load_from_file(os.path.join(env_lmks_dpath, f"{city}.json")).keys())
+            construct_grounded_dataset(args.split_dpath, city_lmks, city, args.remove_perm, args.seed, args.nsamples, args.model, env_dpath)
