104b42bd933d8fb1e1d25153eb677fdf89ad9916
ajshah91 <ankitshah91@gmail.com>
2023-02-01 20:31:58 -0500

    plots on symbolic accuracy

--- plot_results.py
+++ plot_results.py
@@ -11,15 +11,26 @@ from results_analysis import *
 RESULT_DPATH = './figures'
 rc = {'axes.labelsize': 28, 'axes.titlesize': 32, 'legend.fontsize': 24, 'xtick.labelsize': 24, 'ytick.labelsize': 22}
 
-def plot_symbolic_accuracies():
-    df = create_symbolic_accuracies_table(test_types = SYMBOLIC_TEST_TYPES, model_types = ['finetuned_gpt3','s2s_pt_transformer'])
+def plot_symbolic_accuracies1():
+    df = create_symbolic_accuracies_table(test_types = SYMBOLIC_TEST_TYPES, model_types = SYMBOLIC_MODEL_TYPES)
     with sns.plotting_context('poster', rc=rc):
        plt.figure(figsize=[12, 10])
-       sns.barplot(data = df, x = 'Model',y = 'Accuracy',hue = 'Test Type')
+       sns.barplot(data = df, x = 'Model',y = 'Accuracy',hue = 'Test Type', alpha = 0.85, capsize = 0.1)
        plt.ylim([0,1])
        plt.savefig('figures/symbolic_accuracies.jpg', dpi=400, bbox_inches='tight')
        plt.show()
 
+
+def plot_symbolic_accuracies2():
+    df = create_symbolic_accuracies_table(test_types = SYMBOLIC_TEST_TYPES, model_types = SYMBOLIC_MODEL_TYPES)
+    with sns.plotting_context('poster', rc=rc):
+       plt.figure(figsize=[12, 10])
+       sns.barplot(data = df, x = 'Test Type',y = 'Accuracy',hue = 'Model', alpha = 0.85, capsize = 0.1)
+       plt.ylim([0,1])
+       plt.savefig('figures/symbolic_accuracies2.jpg', dpi=400, bbox_inches='tight')
+       plt.show()
+
     
 if __name__ == '__main__':
-    plot_symbolic_accuracies()
+    plot_symbolic_accuracies1()
+    plot_symbolic_accuracies2()
