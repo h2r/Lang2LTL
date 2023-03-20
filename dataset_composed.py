45b44bfdce7d944c4562c2d4702d7c6b7fbf6641
jasonxyliu <xinyu_liu@brown.edu>
2023-03-20 08:26:40 -0400

    rename composed holdout dname, fname

--- dataset_composed.py
+++ dataset_composed.py
@@ -23,29 +23,28 @@ def construct_composed_dataset(data_fpath, composed_fpath):
         dataset["valid_meta"] = composed["meta"]
         dataset["holdout_type"] = holdout_type
 
-        save_dname = os.path.basename(os.path.dirname(data_fpath))
-        save_dpatph = os.path.join("data", f"composed_{holdout_type}_{save_dname}")
+        save_dpatph = os.path.join("data", f"composed_{holdout_type}")
         os.makedirs(save_dpatph, exist_ok=True)
-        save_fname = f"composed_{holdout_type}_{Path(data_fpath).stem}.pkl"
+        save_fname = f"{Path(composed_fpath).stem}.pkl"
         save_fpath = os.path.join(save_dpatph, save_fname)
         save_to_file(dataset, save_fpath)
     elif "formula" in composed_fpath:
         holdout_type = "formula"
-        save_dname = os.path.basename(os.path.dirname(data_fpath))
-        save_dpatph = os.path.join("data", f"composed_{holdout_type}_{save_dname}")
+        save_dpatph = os.path.join("data", f"composed_{holdout_type}")
         os.makedirs(save_dpatph, exist_ok=True)
 
         for train_data, train_meta, valid_data, valid_meta, size, seed, fold_idx in composed:
-            split_fpath = os.path.join(save_dpatph, f"composed_{holdout_type}_{size}_{seed}_fold{fold_idx}.pkl")
+            save_fname = f"{Path(composed_fpath).stem}_{size}_{seed}_fold{fold_idx}.pkl"
+            split_fpath = os.path.join(save_dpatph, save_fname)
             save_split_dataset(split_fpath, train_data, train_meta, valid_data, valid_meta, size, seed)
     elif "utt" in composed_fpath:
         holdout_type = "utt"
-        save_dname = os.path.basename(os.path.dirname(data_fpath))
-        save_dpatph = os.path.join("data", f"composed_{holdout_type}_{save_dname}")
+        save_dpatph = os.path.join("data", f"composed_{holdout_type}")
         os.makedirs(save_dpatph, exist_ok=True)
 
         for train_data, train_meta, valid_data, valid_meta, size, seed in composed:
-            split_fpath = os.path.join(save_dpatph, f"composed_{holdout_type}_{size}_{seed}.pkl")
+            save_fname = f"{Path(composed_fpath).stem}_{size}_{seed}.pkl"
+            split_fpath = os.path.join(save_dpatph, save_fname)
             save_split_dataset(split_fpath, train_data, train_meta, valid_data, valid_meta, size, seed)
     else:
         raise ValueError(f"ERROR: unrecognized holdout type in compose_fpath: {composed_fpath}")
@@ -54,7 +53,7 @@ def construct_composed_dataset(data_fpath, composed_fpath):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_fpath", type=str, default="data/holdout_split_batch12_perm/symbolic_batch12_perm_utt_0.2_0.pkl", help="original symbolic dataset.")
-    parser.add_argument("--composed_fpath", type=str, default="data/composed_zeroshot_symbolic_batch12_noperm.pkl", help="composed dataset.")
+    parser.add_argument("--composed_fpath", type=str, default="data/composed_formula_symbolic_batch12_noperm.pkl", help="composed dataset.")
     args = parser.parse_args()
 
     construct_composed_dataset(args.data_fpath, args.composed_fpath)
