84d74ef4657efd959a499c65bbfa12c126d29c92
jasonxyliu <xinyu_liu@brown.edu>
2023-05-07 02:13:01 -0400

    udpated bash script: conda; root dpath

--- bash_scripts/llama.sh
+++ bash_scripts/llama.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
-#SBATCH --job-name=t5
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH --mem-per-gpu=32G
 #SBATCH --time=1:00:00
 #SBATCH -p 3090-gcondo --gres=gpu:1
 
-module load miniconda
-module load python/3.9.0
-source /gpfs/runtime/opt/miniconda/4.10/etc/profile.d/conda.sh
-conda activate ~/anaconda/lang2ltl
+#SBATCH -e sbatch_out/job-%j.err
+#SBATCH -o sbatch_out/job-%j.out
+
+module load anaconda/2022.05
+source /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
+conda activate lang2ltl
 nvidia-smi
-python llama_example.py
+python /users/$USER/lang2ltl/llama_example.py
