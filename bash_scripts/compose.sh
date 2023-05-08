8cb466feb934d0fb9fbb02b533f3f34cb89c9174
jasonxyliu <xinyu_liu@brown.edu>
2023-05-08 17:44:43 -0400

    added bash for compose

---
+++ bash_scripts/compose.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH --mem=199G
+#SBATCH --time=99:00:00
+
+#SBATCH -e sbatch_out/job-%j.err
+#SBATCH -o sbatch_out/job-%j.out
+
+module load anaconda/2022.05
+source /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
+conda activate lang2ltl
+
+BASE_FPATH="$HOME/data/shared/lang2ltl/data/symbolic_batch12_perm.csv"
+COMPOSED_FPATH="$HOME/data/shared/lang2ltl/data/composed"
+NSAMPLES=5000000
+RATIO=0.6
+ 
+python $HOME/lang2ltl/dataset_composed_new.py --base_fpath BASE_FPATH --composed_dpath COMPOSED_FPATH --nsamples $NSAMPLES --split_ratio $RATIO
