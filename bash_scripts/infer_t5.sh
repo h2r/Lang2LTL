2e248a5de9ba3de924c924050a74c673552fd449
jasonxyliu <xinyu_liu@brown.edu>
2023-05-10 15:04:05 -0400

    test git

--- bash_scripts/infer_t5.sh
+++ bash_scripts/infer_t5.sh
@@ -12,8 +12,8 @@ module load anaconda/2022.05
 source /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
 conda activate lang2ltl
 
-DATA_FPATH="$HOME/data/shared/lang2ltl/data/composed_formula/composed_formula_symbolic_batch12_noperm_900_42_fold0.pkl"
+DATA_FPATH="$HOME/data/shared/lang2ltl/data/composed/split_raito0.6_seed42_sample_nsamples10000_seed42_base_symbolic_batch12_perm.pkl"
 MODEL_FPATH="$HOME/data/shared/lang2ltl/model"
-CKPT=8000
+CKPT=26000
 
 python $HOME/lang2ltl/s2s_sup.py --data_fpath $DATA_FPATH --model_dpath $MODEL_FPATH --model t5-base --checkpoint $CKPT
